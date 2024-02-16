b23a0b4f26bae61710869efec4e3c5091f3addbf
Jose Del Valle <36140117+jdelvalle10@users.noreply.github.com>
2024-02-16 14:18:15 -0500

    Delete VulnPort.py

--- VulnPort.py
+++
@@ -1,9 +0,0 @@
-from portScanner2 import port_scan
-
-target_website = input("[+] Enter target website in format 'www.example.com': ")
-
-open_ports = port_scan(target_website)
-if open_ports:
-    print("Open ports on {}: {}".format(target_website, open_ports))
-else:
-    print("No open ports found on {}.".format(target_website))
